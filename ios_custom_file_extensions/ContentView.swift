76902551ad5aa7fea213e5404caa84b5b1aa2382
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-01-17 11:28:27 +0100

    Initial Commit

---
+++ ios_custom_file_extensions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_custom_file_extensions
+//
+//  Created by Miaomiao Shi on 17/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_custom_file_extensions/ios_custom_file_extensionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_custom_file_extensionsApp.swift
+//  ios_custom_file_extensions
+//
+//  Created by Miaomiao Shi on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ios_custom_file_extensionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
